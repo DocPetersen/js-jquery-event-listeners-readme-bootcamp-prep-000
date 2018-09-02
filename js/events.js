5015e7c2171e01ceb410588bbbd8bd063bdc9077
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-09-02 06:49:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -28,8 +28,6 @@ function pressIt() {
 
 function submitIt() {
   $('form').on('submit'), function() {
-    alert('your form is going to be submitted now');
-    return;
-  }  
-}
+    alert('your form is going to be submitted now')
+  })
 }
